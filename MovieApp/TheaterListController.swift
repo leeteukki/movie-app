8f8704e5fe39bcdd3c292811874cb1cb800efd26
leeteukki <tklohj@naver.com>
2017-06-27 19:57:59 +0900

    jason2

--- MovieApp/TheaterListController.swift
+++ MovieApp/TheaterListController.swift
@@ -32,11 +32,31 @@ class TheaterListController: UITableViewController {
             
             let encdata = stringdata.data(using: String.Encoding.utf8.rawValue)
             
+            do{
+                
+                let apiArray = try JSONSerialization.jsonObject(with: encdata!, options: []) as? NSArray
+                
+                for obj in apiArray! {
+                    self.list.append(obj as! NSDictionary)
+                }
+                
+            } catch {
+                
+                let alert = UIAlertController(title: "실패", message: "데이터 분석이 실패 했습니다", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "확인", style: "데이터 분석이 성공 했습니다", handler: .cancel))
+                self.present(alert, animated: false)
+                
+            }
             
+            self.startPoint += sList
+            
+        } catch {
+            
+            let alert = UIAlertController(title: "실패", message: "데이터를 불러오는데 실패 하였습니다", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "확인", style: .cancel))
+            self.present(alert, animated: false)
             
         }
-        
     }
-    
-    
 }
